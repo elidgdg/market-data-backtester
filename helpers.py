fbca5f27c1b7a9a55cc0e0111311a5a386e790d7
Elijah Gatward <107950467+elidgdg@users.noreply.github.com>
2025-09-17 20:31:11 +0100

    Implement select_price

---
+++ helpers.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pandas as pd
+
+TRADING_DAYS = 252 # Trading days in a year
+
+def select_price(df: pd.DataFrame) -> pd.Series:
+    # Use adjusted if available, otherwise close
+    return df['Adj Close'] if 'Adj Close' in df.columns else df['Close']
